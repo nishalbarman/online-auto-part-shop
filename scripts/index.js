c45f8afad0ec187414d5e4b775a7e1c6a4b65f65
Nishal Barman <nishalbarman@gmail.com>
2023-08-07 02:34:12 +0530

    Home page started

--- scripts/index.js
+++ scripts/index.js
@@ -7,4 +7,10 @@ import {
 window.onload = () => {
   const nav = document.querySelector("#navbar");
   nav.innerHTML = top_navbar() + middle_navbar() + bottom_navbar();
+  const item_cart = document.querySelector("#item_count_cart");
+  let cartItems = localStorage.getItem("cart-total-items") || 0;
+  if (!(cartItems == 0 || cartItems == null)) {
+    item_cart.textContent = cartItems;
+    item_cart.style.display = "flex";
+  }
 };
