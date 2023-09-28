e8ffcd2ecbf41b460f2b0181e2021e9a6f07c871
Nishal Barman <32856353+nishalbarman@users.noreply.github.com>
2023-09-28 10:19:25 +0530

    Update api.js

--- components/api.js
+++ components/api.js
@@ -1,7 +1,7 @@
-const API = `https://autopart.cyclic.cloud`;
+// const API = `https://autopart.cyclic.cloud`;
 // const API = `http://localhost:3000`;
 // const API = `http://3.111.217.106:3000`;
-// const API = `https://json-theta.vercel.app`;
+const API = `https://json-theta.vercel.app`;
 // const API = `https://server-auto.onrender.com`;
 
 export default API;
